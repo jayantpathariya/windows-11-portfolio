e237b6515d641a6775dbb808fc2b845eabfdfc3e
Jayant Pathariya <jayantmehra02@gmail.com>
2024-09-15 11:29:26 +0530

    icon and fonts added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,19 +2,14 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const segoeUI = localFont({
+  src: "./fonts/SegoeUI-VF.ttf",
+  variable: "--font-segoe-ui",
+  weight: "100 300 400 500 600 700 800 900",
 });
 
 export const metadata: Metadata = {
-  title: "Windows 11",
+  title: "Windows 11 Portfolio",
   description:
     "Windows 11 is a modern, sleek, and lightweight operating system.",
 };
@@ -26,11 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${segoeUI.variable} antialiased`}>{children}</body>
     </html>
   );
 }
